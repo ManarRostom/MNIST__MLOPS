b191585eca6433a6e7b307f1c772a116da0ab134
Manar Rostom <manarrosttom2000@gmail.com>
2024-11-02 13:45:24 +0200

    add new code

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# design an mnist machine learning model
+
+from sklearn.datasets import load_digits
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+digits = load_digits()
+
+X = digits.data
+Y = digits.target
+
+model = RandomForestClassifier()
